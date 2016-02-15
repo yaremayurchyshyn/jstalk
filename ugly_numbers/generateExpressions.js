3a6a9b4d4d948fcc9de569d1d00cb3296534cc36
yyurchyshyn <yarema.yurchyshyn@gmail.com>
2016-02-15 12:00:30 +0200

    code refactor

--- ugly_numbers/generateExpressions.js
+++ ugly_numbers/generateExpressions.js
@@ -2,15 +2,20 @@
 console.log('generateExpressions');
 
   function generateExpression(exp) {
-    var operations = ["+", "-", ""];
-    var generatedExpressions = [];
-    var str = exp.split("");
-    var operationsCount = str.length - 1;
+    var operations;
+    var generatedExpressions;
+    var str;
+    var operationsCount;
 
     _init();
 
     function _init() {
-      generatedExpressions.push([str[0]]);
+      operations = ["+", "-", ""]; //array of possible operations
+      generatedExpressions = []; //array which return all possible expressions
+      str = exp.split(""); //splited string (e.g. ["1", "2", "3", .., "n"])
+      operationsCount = str.length - 1; //how much operations will be provided, used for recursive function "generateNewExpression"
+
+      generatedExpressions.push([str[0]]); // init generatedExpressions with first symbol from input string
     }
 
     function generateNewExpression() {
@@ -18,14 +23,12 @@ console.log('generateExpressions');
         var newGeneratedExpressions = [];
 
         for(var i= 0, leni=generatedExpressions.length; i<leni; i++) {
-          var temp = [];
           for(var j= 0, lenj=operations.length; j<lenj; j++) {
             var newArr = generatedExpressions[i].slice();
             newArr.push(operations[j]);
             newArr.push(str[str.length - (operationsCount)]);
-            temp.push(newArr);
+            newGeneratedExpressions.push(newArr);
           }
-          newGeneratedExpressions = newGeneratedExpressions.concat(temp);
         }
         generatedExpressions = newGeneratedExpressions;
         operationsCount --;
@@ -37,9 +40,8 @@ console.log('generateExpressions');
     return generateNewExpression();
   }
 
-  console.log(generateExpression('123456789'));
-  console.log(generateExpression('123'));
-  console.log(generateExpression('1234'));
+  var result = generateExpression('12345');
+  console.log(result);
 
 })();
 
